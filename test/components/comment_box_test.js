b6a12327e1a06885dc768444fc3ee1e29e87a430
Rahilka Simonova <rahilka.simonova@yahoo.com>
2017-07-19 18:29:46 +0200

    Comment box test

--- test/components/comment_box_test.js
+++ test/components/comment_box_test.js
@@ -3,7 +3,8 @@ import CommentBox from '../../src/components/comment_box';
 
 describe('CommentBox', () => {
 	it('has a text area', () => {
-
+		const component = renderComponent(CommentBox);
+		expect(component.find('textarea')).to.exist;
 	});
 
 	it('has a button', () => {
