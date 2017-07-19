78fe986964658ac28587664971725eec415aee53
Rahilka Simonova <rahilka.simonova@yahoo.com>
2017-07-19 16:00:01 +0200

    A first spec

--- test/components/app_test.js
+++ test/components/app_test.js
@@ -4,10 +4,17 @@ import { renderComponent, expect } from '../test_helper';
 import App from '../../src/components/app';
 
 // Use 'describe' to group together similar tests, and pass whatever string we want, it will appear in the report after testing
-describe('App')
+describe('App', () => {
+		
+	// Use 'it' to test a single attribute of a target, for one single test
+	it('shows the correct text', () => {
 
-// Use 'it' to test a single attribute of a target, for one single test
-it('shows the correct text')
+		// create an instance of App
+		const component = renderComponent(App);
 
-// Use 'expect' to make an 'assertion' about a target
-expect
+		// Use 'expect' to make an 'assertion' about a target
+		expect(component).to.contain('React simple starter');
+
+	});
+
+});
